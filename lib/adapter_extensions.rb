6b738b1420b415e9f8612b7fdc2c9c435550b6f2
Colin Darie <colin@darie.eu>
2017-12-21 16:05:33 +0100

    Module#prepend instead of alias_method_chain

--- lib/adapter_extensions.rb
+++ lib/adapter_extensions.rb
@@ -1,18 +1,13 @@
 require 'adapter_extensions/base'
 
-class ActiveRecord::Base
-  class << self
-    # for 1.8.7, modules loaded more than once that use alias_method_chain will 
-    # cause stack level too deep errors - make sure we avoid this
-    unless self.instance_methods.include?('establish_connection_without_adapter_extensions')
-      def establish_connection_with_adapter_extensions(*args)
-        establish_connection_without_adapter_extensions(*args)
-        ActiveSupport.run_load_hooks(:active_record_connection_established, connection_pool)
-      end
-      alias_method_chain :establish_connection, :adapter_extensions
-    end
+module ActiveRecordWithAdapterExtensions
+  def establish_connection(*args)
+    super(*args)
+    ActiveSupport.run_load_hooks(:active_record_connection_established, connection_pool)
   end
 end
+
+ActiveRecord::Base.singleton_class.prepend ActiveRecordWithAdapterExtensions
 
 ActiveSupport.on_load(:active_record_connection_established) do |connection_pool|
   AdapterExtensions.load_from_connection_pool connection_pool
